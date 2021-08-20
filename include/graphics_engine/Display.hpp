a5b589ab1dc44f52ebe549db55774a8f662dfeb2
alexnavtt <alexnavtt@utexas.edu>
2021-08-20 00:13:28 -0500

    initial commit

---
+++ include/graphics_engine/Display.hpp
@@ -0,0 +1,13 @@
+#ifndef DISPLAY_HPP_
+#define DISPLAY_HPP_
+
+class Window{
+public:
+    Window();
+
+    void init();
+private:
+
+};
+
+#endif
---
+++ src/Display.cpp
@@ -0,0 +1,10 @@
+#include "graphics_engine/Display.hpp"
+#include "GL/glew.h"
+#include "GLFW/glfw3.h"
+
+Window::Window(){
+
+}
+
+void Window::init(){
+}
