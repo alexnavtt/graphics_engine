c62a74d9c57371d806d63422006cf535cd025d61
alexnavtt <alexnavtt@utexas.edu>
2021-08-24 20:15:40 -0500

    Cleanup

--- include/graphics_engine/Display.hpp
+++ include/graphics_engine/Display.hpp
@@ -9,31 +9,36 @@
 #include <GL/glew.h>
 #include "GLFW/glfw3.h"
 
-typedef std::array<GLfloat,3> Point;
-typedef std::array<GLfloat,3> Vec;
+#include "graphics_engine/Line.hpp"
+#include "graphics_engine/Traingle.hpp"
 
-struct Triangle{
-    // Triangle(std::vector<GLfloat>& verts){
-    //     memcpy(vertices, verts.data(), sizeof(verts.data()));
-    // }
-
-    GLfloat vertices[9] = {0, 0, 0, 0, 0, 0, 0, 0, 0};
-    Vec normal;
-};
 
 class Window{
 public:
+    // Constructor: Great a GL Window
     Window();
 
+    // Close the window
     void close();
 
+    // Draw a triangle in the window. Requires a followup call to render()
     void drawTriangle(Triangle& T);
 
+    void drawLine(const Line& L);
+
+    // Set the window background colour. Requires a followup call to render()
     void setBackgroundColor(float r, float g, float b, float a);
 
+    // Apply all changes since the last render to the window
     void render();
+
 private:
+    // Pointer to the window instance
     GLFWwindow* window_;
+
+    // ID For Shaders
+    GLuint vertex_shader_id_;
+    GLuint fragment_shader_id_;
 };
 
 #endif
