b0bd95ca26ec30438ac4835fe98c9051d335eed6
alexnavtt <alexnavtt@utexas.edu>
2021-08-24 20:15:30 -0500

    Added shading

--- src/Display.cpp
+++ src/Display.cpp
@@ -10,6 +10,7 @@ using std::cout;
 using std::endl;
 
 Window::Window(){
+    // --- Start GLFW (Window Library) --- //
     if (!glfwInit()){
         std::cout << "Whoops, didn't work" << std::endl;  
     }else{
@@ -18,17 +19,53 @@ Window::Window(){
 
 	// Open a window and create its OpenGL context
 	window_ = glfwCreateWindow( 1024, 768, "Graphics Engine", NULL, NULL);
+    if (!window_){
+        throw DisplayException();
+    }
     glfwMakeContextCurrent(window_);
 
+
+
+
+    // --- Start GLEW (Drawing Library) --- //
     glewInit();
- 
-    // vertex_shader = glCreateShader(GL_VERTEX_SHADER);
-    // glShaderSource(vertex_shader, 1, &vertex_shader_text, NULL);
-    // glCompileShader(vertex_shader);
- 
-    // fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
-    // glShaderSource(fragment_shader, 1, &fragment_shader_text, NULL);
-    // glCompileShader(fragment_shader);
+
+
+
+    // --- Install shaders --- //
+
+    // Load the vertex shader
+    vertex_shader_id_ = glCreateShader(GL_VERTEX_SHADER);
+    const char* vertex_shader_code[1] = {
+    #include "graphics_engine/2DVertexShader"
+    };
+    glShaderSource(vertex_shader_id_, 1, vertex_shader_code, 0);
+    glCompileShader(vertex_shader_id_);
+
+    // Load the fragment shader
+    fragment_shader_id_ = glCreateShader(GL_FRAGMENT_SHADER);
+    const char* fragment_shader_code[1] = {
+    #include "graphics_engine/2DFragmentShader"
+    };
+    glShaderSource(fragment_shader_id_, 1, fragment_shader_code, 0);
+    glCompileShader(fragment_shader_id_);
+
+    GLuint program_id = glCreateProgram();
+    glAttachShader(program_id, vertex_shader_id_);
+    glAttachShader(program_id, fragment_shader_id_);
+    glLinkProgram(program_id);
+    glUseProgram(program_id);
+
+    // Check Vertex Shader
+    GLint Result = GL_FALSE;
+    int InfoLogLength;
+	glGetShaderiv(vertex_shader_id_, GL_COMPILE_STATUS, &Result);
+	glGetShaderiv(vertex_shader_id_, GL_INFO_LOG_LENGTH, &InfoLogLength);
+	if ( InfoLogLength > 0 ){
+		std::vector<char> VertexShaderErrorMessage(InfoLogLength+1);
+		glGetShaderInfoLog(vertex_shader_id_, InfoLogLength, NULL, &VertexShaderErrorMessage[0]);
+		printf("%s\n", &VertexShaderErrorMessage[0]);
+	}
 }
 
 void Window::close(){
@@ -46,24 +83,38 @@ void Window::render(){
     glClear( GL_COLOR_BUFFER_BIT );
 }
 
+void Window::drawLine(const Line& L){
+    float data[] = {L.p1.x, L.p1.y, L.p2.x, L.p2.y};
+
+    GLuint buffer_id;
+    glGenBuffers(1, &buffer_id);
+    glBindBuffer(GL_ARRAY_BUFFER, buffer_id);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(data), data, GL_STATIC_DRAW);
+
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 0, 0);
+
+    glDrawArrays(GL_LINES, 0, 2);
+    glDisableVertexAttribArray(0);
+}
+
 void Window::drawTriangle(Triangle& T){
     GLuint vertex_buffer;
     glGenBuffers(1, &vertex_buffer);
     glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer);
     glBufferData(GL_ARRAY_BUFFER, sizeof(T.vertices), T.vertices, GL_STATIC_DRAW);
 
+    // Enable vertex array
     glEnableVertexAttribArray(0);
-    glVertexAttribPointer(
-        0,                  // attribute 0. No particular reason for 0, but must match the layout in the shader.
-        3,                  // size
-        GL_FLOAT,           // type
-        GL_FALSE,           // normalized?
-        0,                  // stride
-        (void*)0            // array buffer offset
-    );
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float)*6, (void*)(0));
+
+    // Enable colour array
+    glEnableVertexAttribArray(1);
+    glVertexAttribPointer(1, 4, GL_FLOAT, GL_FALSE, sizeof(float)*6, (void*)(sizeof(float)*2));
 
     glDrawArrays(GL_TRIANGLES, 0, 3); // 3 indices starting at 0 -> 1 triangle
-    glDisableVertexAttribArray(0);
+    // glDisableVertexAttribArray(0);
+    // glDisableVertexAttribArray(1);
 }
 
 int main(){
@@ -72,8 +123,10 @@ int main(){
     win.render();
     usleep(1000000);
     
-    Triangle tri{{0.0, -0.9, 0.0, 0.9, 0.9, 0.0, -0.9, 0.9, 0.0}};
+    Triangle tri(0.0, -0.9, 0.9, 0.9, -0.9, 0.9);
     win.drawTriangle(tri);
+    Line L{0, 0, 0.5, 0.5};
+    // win.drawLine(L);
     win.render();
     usleep(1000000);
     
