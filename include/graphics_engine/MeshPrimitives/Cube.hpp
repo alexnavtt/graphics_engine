9a7bb40bb0b3a1dab9e06a657ff0d43991c50d58
alexnavtt <alexnavtt@utexas.edu>
2021-11-23 11:15:05 -0600

    Added outline line to the cube

--- include/graphics_engine/MeshPrimitives/Cube.hpp
+++ include/graphics_engine/MeshPrimitives/Cube.hpp
@@ -8,6 +8,7 @@ public:
     Cube(float size, Point3D loc){
         const float L = 0.5*size;
 
+        points.reserve(8);
         points.push_back({ L,  L,  L});
         points.push_back({-L,  L,  L});
         points.push_back({-L, -L,  L});
@@ -17,10 +18,14 @@ public:
         points.push_back({-L, -L, -L});
         points.push_back({ L, -L, -L});
 
+        outline = {0,1,  1,2,  2,3,  3,0,
+                   0,4,  3,7,  1,5,  2,6,
+                   4,5,  5,6,  6,7,  7,4};
+
         indices = {0, 1, 3,   1, 2, 3,   0, 3, 4,   3, 7, 4, 
-                        5, 4, 7,   7, 6, 5,   1, 5, 6,   6, 2, 1,
-                        0, 4, 1,   4, 5, 1,   2, 3, 7,   7, 6, 2};
-        use_element_array = true;
+                   5, 4, 7,   7, 6, 5,   1, 5, 6,   6, 2, 1,
+                   0, 4, 1,   4, 5, 1,   2, 3, 7,   7, 6, 2};
+
     }
 };
 
