53d88c11af13f595b79ecdd119b38a2a19fb9093
alexnavtt <alexnavtt@utexas.edu>
2021-11-23 11:16:13 -0600

    added more customization options to mesh

--- include/graphics_engine/ThreeDim/Mesh.hpp
+++ include/graphics_engine/ThreeDim/Mesh.hpp
@@ -2,6 +2,7 @@
 #define MESH_HPP_
 
 #include <vector>
+#include "GL/glew.h"
 #include "graphics_engine/base/Transforms.hpp"
 #include "graphics_engine/ThreeDim/Orientation.hpp"
 #include "graphics_engine/ThreeDim/Triangle3D.hpp"
@@ -12,8 +13,12 @@ public:
     std::vector<Triangle3D> triangles;
     std::vector<Point3D> points;
     std::vector<GLuint> indices;
-    bool use_element_array = false;
+    std::vector<GLuint> outline;
+
+    bool shade_faces = true;
+    bool use_element_array = true;
     bool draw_outline = false;
+    bool use_outline_colour = false;
 };
 
 #endif
